7f9e17673a5ddc36ba97252ce167fe1de2da994a
pentwo <pentwo@gmail.com>
2021-03-09 12:17:26 +0800

    fix: google map iframe title

--- src/components/Location.js
+++ src/components/Location.js
@@ -34,9 +34,8 @@ export default function Location() {
         <div className="map">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3385.2905486118393!2d115.87243435184001!3d-31.95301352951979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2bcd579c983e6cb1%3A0x770e6a6a926626cd!2sPure%20Bodywork%20Massage%20%26%20Beauty!5e0!3m2!1sen!2sau!4v1614394945411!5m2!1sen!2sau"
-            // height="450"
+            title="google map"
             style={{ border: 0, width: '100%', height: '350px' }}
-            // allowfullscreen="true"
             loading="lazy"
           ></iframe>
         </div>
