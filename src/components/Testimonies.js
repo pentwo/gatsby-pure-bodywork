67a143950da07ce93a0d3d9a20a7f7059e55498c
pentwo <pentwo@gmail.com>
2021-03-13 16:12:41 +0800

    fix: organize data source

--- src/components/Testimonies.js
+++ src/components/Testimonies.js
@@ -2,8 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Slider from 'react-slick'
 
-import maleAvatar from '../images/icons/icons8-circled-user-male-skin-type-1-2-100.png'
-import femaleAvatar from '../images/icons/icons8-circled-user-female-skin-type-3-100.png'
+import { testimoniesData } from '../data/testimoniesData'
 
 const sliderSettings = {
   dots: false,
@@ -83,7 +82,7 @@ export const Testimonies = () => {
       <div className="wrapper">
         <h3 className="center">What People Say</h3>
         <Slider {...sliderSettings}>
-          {data.map(item => {
+          {testimoniesData.map(item => {
             return (
               <Card
                 name={item.name}
@@ -99,58 +98,3 @@ export const Testimonies = () => {
     </TestimoniesStyles>
   )
 }
-
-const data = [
-  {
-    id: 1,
-    name: 'John Searle',
-    avatar: maleAvatar,
-    comment:
-      "What a wonderful personalised and caring experience I had with Monica with a 90min massage. I've had plenty of massages in my time and it was a real pleasure to feel important and my needs catered for with the massage. I would recommend anybody to come and experience the joy of having a massage with Monica at Pure Bodywork, I am already looking forward to my next visit.",
-    shortComment: 'What a wonderful personalised and caring experience!',
-  },
-  {
-    id: 2,
-    name: 'Ellaine Din',
-    avatar: femaleAvatar,
-    comment:
-      "I cannot believe how amazing monica's hand-works! She definitely have some magic's on her hands! She took all my body and muscles pains away, I feel so much lighter. No more headaches! She is the best massager I have ever tried, and I cannot wait to see her again ! Very professional.. HIGHLY RECOMMENDED !",
-    shortComment:
-      "She definitely have some magic's on her hands! She took all my body and muscles pains away",
-  },
-  {
-    id: 3,
-    name: 'Jalene Van',
-    avatar: femaleAvatar,
-    comment:
-      'Oh my goodness..... what a fantastic experience! She customized our massage to suit our needs. Her magic hands and fingers did the trick and I felt amazing afterwards. Monica is a lovely lady with a warm personality. What a pleasure it was meeting her.',
-    shortComment:
-      'She customized our massage to suit our needs. Her magic hands and fingers did the trick and I felt amazing afterwards.',
-  },
-  {
-    id: 4,
-    name: 'Li Chu',
-    avatar: femaleAvatar,
-    comment:
-      'This is my second time to get a massage here. The atmosphere of the shop is very cozy. Monica is very attentive and professional. I feel much better. She got English and Mandarin language abilities. So highly recommend her to everyone. ',
-    shortComment:
-      'The atmosphere of the shop is very cozy. Monica is very attentive and professional.',
-  },
-  {
-    id: 5,
-    name: 'LokSze Lee',
-    avatar: femaleAvatar,
-    comment:
-      'Monica is very nice, massage and facial are very great ! Highly recommend',
-    shortComment:
-      'Monica is very nice, massage and facial are very great ! Highly recommend',
-  },
-  {
-    id: 6,
-    name: 'Stephanie Huang',
-    avatar: femaleAvatar,
-    comment:
-      "Had an excellent massage! Was so sore in a few different areas, from my neck, to my back and down to my legs! But after the massage and a good night's rest, I feel so much better! Thank you!",
-    shortComment: "Had an excellent massage and a good night's rest",
-  },
-]
