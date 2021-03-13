6a3a63857cd4cc3eb3dc88e181be9674038b17d7
pentwo <pentwo@gmail.com>
2021-03-13 16:12:17 +0800

    fix: organize data source

--- src/data/treatmentsData.js
+++ src/data/treatmentsData.js
@@ -1,94 +1,13 @@
-import React from 'react'
-import Collapsible from 'react-collapsible'
-import styled from 'styled-components'
+import remedialBg from '../images/treatments/remedial.jpg'
+import relaxationBg from '../images/treatments/relaxation.jpg'
+import deepTissueBg from '../images/treatments/deepTissue.jpg'
+import aromatherapyBg from '../images/treatments/aromatherapy.jpg'
+import pregnancyBg from '../images/treatments/pregnancy.jpg'
+import hotStoneBg from '../images/treatments/hotStone.jpg'
+import coupleBg from '../images/treatments/couple.jpg'
+import singingBowlBg from '../images/treatments/singingBowl.jpg'
 
-import { treatmentsData } from '../data/treatmentsData'
-
-import { PageHero } from '../components/Hero'
-import SEO from '../components/SEO'
-
-import { IoIosArrowDropup } from 'react-icons/io'
-
-const CollapsibleStyles = styled.div`
-  .Collapsible {
-    margin-bottom: 3rem;
-    &__trigger {
-      h3 {
-        margin-left: 3rem;
-        margin-bottom: 0;
-      }
-      &.is-open {
-        min-height: 256px;
-        .icon {
-          top: 104px;
-          transform: rotateZ(180deg);
-        }
-      }
-
-      position: relative;
-      border-radius: 10px;
-      display: flex;
-      align-items: center;
-
-      min-height: 128px;
-      background-image: linear-gradient(
-          to right,
-          var(--gradient-primary),
-          var(--gradient-secondary)
-        ),
-        ${props => `url(${props.bg})`};
-      background-size: cover;
-      background-position: center center;
-      background-repeat: no-repeat;
-
-      transition: min-height 300ms;
-      .icon {
-        height: 48px;
-        width: 48px;
-        position: absolute;
-        right: 3rem;
-        top: 40px;
-        display: block;
-        transition: all 300ms;
-      }
-    }
-  }
-`
-
-export default function TreatmentsPage() {
-  return (
-    <main>
-      <SEO title="Treatments"></SEO>
-
-      <PageHero title="Treatments" />
-
-      <div className="wrapper-column">
-        {treatmentsData.map(treatment => {
-          return (
-            <CollapsibleStyles bg={treatment.image}>
-              <Collapsible
-                key={`treatment-${treatment.id}`}
-                trigger={
-                  <>
-                    <h3>{treatment.name}</h3>
-                    <IoIosArrowDropup className="icon" />
-                  </>
-                }
-              >
-                <div
-                  className="description"
-                  dangerouslySetInnerHTML={{ __html: treatment.description }}
-                />
-              </Collapsible>
-            </CollapsibleStyles>
-          )
-        })}
-      </div>
-    </main>
-  )
-}
-/*
-const treatmentsData = [
+export const treatmentsData = [
   {
     id: 1,
     name: 'Remedial Massage',
@@ -191,4 +110,3 @@ const treatmentsData = [
     // Photo by Magicbowls from Pexels
   },
 ]
-*/
