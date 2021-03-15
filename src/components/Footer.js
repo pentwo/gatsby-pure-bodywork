62e4fa60766e62284e8a1aa0bbb1c48f5acbed62
pentwo <pentwo@gmail.com>
2021-03-15 23:18:33 +0800

    feat: update footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 import { AiFillPhone, AiFillMail } from 'react-icons/ai'
+import { Link } from 'gatsby'
 
 const FooterStyles = styled.div`
   padding: 3rem 10vw;
-  /* width: 100%; */
+
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 2rem;
+  grid-template-columns: 1fr auto 1fr;
+  gap: 3rem;
 
   background: var(--light);
 
-  align-items: center;
   font-size: 1.5rem;
 
   ul {
     list-style: none;
+    padding: 0;
   }
   li {
     margin-bottom: 0.5rem;
+    display: flex;
+    justify-content: space-between;
   }
 
   .hours {
-    grid-row: 1/3;
+    /* grid-row: 1/3; */
   }
   .copyrights {
-    grid-column: span 2;
+    grid-column: span 3;
   }
   .phone,
   .email {
@@ -44,8 +47,9 @@ const FooterStyles = styled.div`
 
   @media (max-width: 640px) {
     grid-template-columns: 1fr;
+    grid-template-rows: 1fr auto 1fr;
     .hours {
-      grid-row: 1/2;
+      /* grid-row: 1/2; */
     }
     .copyrights {
       grid-column: 1;
@@ -59,15 +63,34 @@ export default function Footer() {
       <div className="hours">
         <h5>Business Hours</h5>
         <ul>
-          <li>Monday 9:00 am - 6:30 pm</li>
           <li>
-            <span className="mark">Tuesday Closed</span>
+            <span>Monday</span>
+            <span>9:00 am - 6:30 pm</span>
           </li>
-          <li>Wednesday 9:00 am - 6:30 pm </li>
-          <li>Thursday 9:00 am - 6:30 pm </li>
-          <li>Friday 9:00 am - 5:00 pm</li>
-          <li>Saturday 10:00 am - 2:00 pm</li>
-          <li>Sunday 11:00 am - 4:00 pm</li>
+          <li className="mark">
+            <span>Tuesday</span>
+            <span>Closed</span>
+          </li>
+          <li>
+            <span>Wednesday</span>
+            <span>9:00 am - 6:30 pm</span>
+          </li>
+          <li>
+            <span>Thursday</span>
+            <span>9:00 am - 6:30 pm</span>
+          </li>
+          <li>
+            <span>Friday</span>
+            <span>9:00 am - 5:00 pm</span>
+          </li>
+          <li>
+            <span>Saturday</span>
+            <span>10:00 am - 2:00 pm</span>
+          </li>
+          <li>
+            <span>Sunday</span>
+            <span>11:00 am - 4:00 pm</span>
+          </li>
         </ul>
       </div>
 
@@ -86,6 +109,26 @@ export default function Footer() {
         </div>
       </div>
 
+      <div className="sitemap">
+        <h5>Sitemap</h5>
+        <ul>
+          <li>
+            <Link to="/team">OUR TEAM</Link>
+          </li>
+          <li>
+            <Link to="/treatments">TREATMENTS</Link>
+          </li>
+          <li>
+            <Link to="/price">PRICE</Link>
+          </li>
+          <li>
+            <Link to="/qna">Q&A</Link>
+          </li>
+          <li>
+            <Link to="/contact">CONTACT</Link>
+          </li>
+        </ul>
+      </div>
       <div className="copyrights center">
         <p>&copy; Code by Pen</p>
       </div>
