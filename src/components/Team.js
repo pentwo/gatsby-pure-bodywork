4c72b0b90bfbfc0604ef3dfb1bf9cd6fb9bdbc45
pentwo <pentwo@gmail.com>
2021-03-18 11:41:55 +0800

    feat: add new team member

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import monicaAvatar from '../images/avatar.jpg'
+import { members } from '../data/utility'
 
 const TeamStyles = styled.section`
   /* padding: 3rem 10vw; */
@@ -16,7 +16,7 @@ const TeamStyles = styled.section`
   .wrapper {
     display: grid;
     /* grid-template-columns: repeat(auto-fill, minmax(min(35rem, 100%), 1fr)); */
-    grid-template-columns: minmax(min(35rem, 45rem));
+    grid-template-columns: repeat(2, 1fr);
     align-items: center;
     justify-content: space-between;
     gap: 5rem;
@@ -24,6 +24,7 @@ const TeamStyles = styled.section`
 
   .member {
     text-align: center;
+    align-self: start;
   }
   article {
   }
@@ -47,7 +48,7 @@ export const Team = () => {
       <h3 className="center">About Us</h3>
       <div className="wrapper">
         {members.map(member => (
-          <div className="member" key={member.id}>
+          <div className="member" key={member.name}>
             <img src={member.avatarImage} alt="" />
             <h3>{member.name}</h3>
             <article>
@@ -59,40 +60,3 @@ export const Team = () => {
     </TeamStyles>
   )
 }
-
-/*
-<p>
-  After 10 years of experience in the massage industry, as part of
-  my practice I combine the techniques of Traditional Chinse
-  Medicine with Five Element acupuncture and believe in treating the
-  underlying cause of the symptoms, helping to lead to a more
-  permanent health solution.
-</p>
-<p>
-  I believe that in order to be great in the world you must love and
-  nourish your body. As a woman living and working in a fast-paced,
-  demanding urban environment, I know the pressure to succeed, look
-  good and live up to our own high expectations can be overwhelming.
-</p>
-<p>
-  We can get so focused on our outside environment that we lose
-  touch with our bodies and ourselves. And yet that’s exactly where
-  I’ve discovered we need to start in order to truly take care of
-  ourselves. ​
-</p>
-<p>
-  It’s my goal to create space for others to discover their own
-  inner power and wisdom so they can emerge as the greatest version
-  of themselves.
-</p>
-*/
-
-const members = [
-  {
-    id: 1,
-    name: 'Monica',
-    avatarImage: monicaAvatar,
-    description:
-      'I am passionate about helping others to improve their well-being by offering relaxing and rejuvenating treatments that nurture the self and lead to long term health solutions. I believe in practicing holistically and focusing on both your physical and emotional needs, establishing a balance between the mind and body.',
-  },
-]
--- src/data/utility.js
+++ src/data/utility.js
@@ -1,3 +1,5 @@
+import monicaAvatar from '../images/avatar.jpg'
+
 export const BOOKING_URL =
   'https://perthhealthcare.au1.cliniko.com/bookings?business_id=74448&practitioner_id=159109'
 
@@ -9,4 +11,23 @@ export const businessHour = [
   { day: 'Friday', hour: '8.00 am - 8.00 pm' },
   { day: 'Saturday', hour: '8.00 am - 6.00 pm' },
   { day: 'Sunday / Public Holiday', hour: '8.00 am - 6.00 pm' },
+]
+
+export const members = [
+  {
+    name: 'Monica',
+    avatarImage: monicaAvatar,
+    description:
+      'I am passionate about helping others to improve their well-being by offering relaxing and rejuvenating treatments that nurture the self and lead to long term health solutions.',
+    fullDescription:
+      '<p>I am passionate about helping others to improve their well-being by offering relaxing and rejuvenating treatments that nurture the self and lead to long term health solutions. I believe in practicing holistically and focusing on both your physical and emotional needs, establishing a balance between the mind and body.</p><p>After 10 years of experience in the massage industry, as part of my practice I combine the techniques of Traditional Chinese Medicine with Five Element acupuncture and believe in treating the underlying cause of the symptoms, helping to lead to a more permanent health solution.</p><p>I believe that in order to be great in the world you must love and nourish your body. As a woman living and working in a fast-paced, demanding urban environment, I know the pressure to succeed, look good and live up to our own high expectations can be overwhelming.</p><p>We can get so focused on our outside environment that we lose touch with our bodies and ourselves. And yet that’s exactly where I’ve discovered we need to start in order to truly take care of ourselves. </p><p>It’s my goal to create space for others to discover their own inner power and wisdom so they can emerge as the greatest version of themselves.</p>',
+  },
+  {
+    name: 'Emily Yeh',
+    avatarImage: monicaAvatar,
+    description:
+      'Emily is fully trained, and has a diploma in remedial massage therapy. She is registered and has a provider number for most private health funds.',
+    fullDescription:
+      '<p>Emily is fully trained, and has a diploma in remedial massage therapy. She is registered and has a provider number for most private health funds.</p><p>Emily specialises in relaxation, deep tissue, remedial, sport and hot stone massage. She is kind, friendly and passionate about her work.</p><p>Emily always does her best to meet her clients needs and her goal is to make you leave our clinic satisfied.</p>',
+  },
 ]
