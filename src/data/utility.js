afc27eba1e5e10d4003ec17057ab68fec733b454
pentwo <pentwo@gmail.com>
2021-03-20 17:51:44 +0800

    feat: update avatar

--- src/data/utility.js
+++ src/data/utility.js
@@ -1,4 +1,5 @@
-import monicaAvatar from '../images/avatar.jpg'
+import monicaAvatar from '../images/avatar-monica.jpg'
+import emilyAvatar from '../images/avatar-emily.jpg'
 
 export const BOOKING_URL =
   'https://perthhealthcare.au1.cliniko.com/bookings?business_id=74448&practitioner_id=159109'
@@ -15,19 +16,25 @@ export const businessHour = [
 
 export const members = [
   {
-    name: 'Monica',
+    id: 159109,
+    name: 'Monica Li',
     avatarImage: monicaAvatar,
     description:
       'I am passionate about helping others to improve their well-being by offering relaxing and rejuvenating treatments that nurture the self and lead to long term health solutions.',
     fullDescription:
-      '<p>I am passionate about helping others to improve their well-being by offering relaxing and rejuvenating treatments that nurture the self and lead to long term health solutions. I believe in practicing holistically and focusing on both your physical and emotional needs, establishing a balance between the mind and body.</p><p>After 10 years of experience in the massage industry, as part of my practice I combine the techniques of Traditional Chinese Medicine with Five Element acupuncture and believe in treating the underlying cause of the symptoms, helping to lead to a more permanent health solution.</p><p>I believe that in order to be great in the world you must love and nourish your body. As a woman living and working in a fast-paced, demanding urban environment, I know the pressure to succeed, look good and live up to our own high expectations can be overwhelming.</p><p>We can get so focused on our outside environment that we lose touch with our bodies and ourselves. And yet that’s exactly where I’ve discovered we need to start in order to truly take care of ourselves. </p><p>It’s my goal to create space for others to discover their own inner power and wisdom so they can emerge as the greatest version of themselves.</p>',
+      "<p>Monica's career in massage began in 2008,  she is enthusiastic and passionate about all things massage therapy.</p><p>Having studied vastly in the field, her amazing hands-on massage techniques combined with her Chinese Medicine understanding of the body makes her a Remedial Massage Therapist you can trust.</p><p>She has expertise in remedial massage, deep tissue massage, pregnancy massage, trigger point therapy, swedish massage, cupping and beauty therapy.</p><h5>Qualification Details</h5><ul><li>Massage association of Austalia</li><li>Diploma of Remedial Massage</li><li>Reiki First degree</li><li>Tibatan Singing bowl sounds therapy Level One & Two</li><li>NAHA Certified Professional Aromatherapist Level 2</li><li>First aid – Level 2</li></ul>",
+    booking:
+      'https://perthhealthcare.au1.cliniko.com/bookings?practitioner_id=159109',
   },
   {
+    id: 160950,
     name: 'Emily Yeh',
-    avatarImage: monicaAvatar,
+    avatarImage: emilyAvatar,
     description:
       'Emily is fully trained, and has a diploma in remedial massage therapy. She is registered and has a provider number for most private health funds.',
     fullDescription:
       '<p>Emily is fully trained, and has a diploma in remedial massage therapy. She is registered and has a provider number for most private health funds.</p><p>Emily specialises in relaxation, deep tissue, remedial, sport and hot stone massage. She is kind, friendly and passionate about her work.</p><p>Emily always does her best to meet her clients needs and her goal is to make you leave our clinic satisfied.</p>',
+    booking:
+      'https://perthhealthcare.au1.cliniko.com/bookings?practitioner_id=160950',
   },
 ]
