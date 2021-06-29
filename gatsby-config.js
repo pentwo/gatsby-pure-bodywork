aceb0fa31350368a8878cd73f61abf6fe39a934c
pentwo <pentwo@gmail.com>
2021-06-29 21:02:12 +0800

    feat: add google gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,20 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-1EM087JR2X', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      },
+    },
   ],
 }
